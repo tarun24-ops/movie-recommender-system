7482f2f6e78aa192d5e30d2bc01585c0f5ca723a
tarun-24ops <tarunsai1563@gmail.com>
2024-10-04 11:57:31 +0530

    Final-Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # movie-recommender-system
 This repository will focus on building movie recommender system using LLMs
+
+## CREATED VIRTUAL ENIVIRONMENT
+* Used '''python -m venv .venv to create the Virtual Environmnet
+* Activating the Virtual Environment 
+
+## Install Requirements
+* pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,42 @@
+##IMPORTING the libraries
+import streamlit as st 
+import google.generativeai as genai 
+from langchain import PromptTemplate,LLMChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+
+## Set up local environment
+import os 
+from dotenv import load_dotenv 
+load_dotenv() #ACtivate the local envirionment
+genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
+
+import os
+os.environ['GOOGLE_API_KEY'] = "AIzaSyAzOfotbdOxJ6LJTht8daCw78p9yqgdW1U"  # Replace with your actual API key
+
+
+
+## Designing the webpage
+st.title("🎦MARS(Automated Movie Recommender System)")
+user_input=st.text_input("Enter The Movie Title,\Genre or Keyword🎞️")
+
+# Create a prompt template for generating movie recommendations
+prompt_template = PromptTemplate(
+    input_variables=["user_input"],
+    template="Based on the input '{user_input}', suggest some movies that I might enjoy.")
+
+# Initialize the generative AI model
+llm = ChatGoogleGenerativeAI(model="gemini-pro",api_key=os.getenv("GOOGLE-API-KEY"))
+
+# When the user submits their input
+if user_input:
+    
+    # Format the prompt with user input
+    prompt = prompt_template.format(user_input=user_input)
+    recommendations=llm.predict(text=prompt)
+    # Display the recommendations in the Streamlit app
+    st.write(f"Recommended Movies:\n{recommendations}")
+else:
+    st.warning("Please enter a movie title, genre, or keyword.")
+        
